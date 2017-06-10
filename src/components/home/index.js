ad382937c0a938f91d5cbf6cb9574a626314db97
Dávid Szabó <david.szabo97@gmail.com>
2017-06-10 22:38:31 +0200

    Make home like home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,120 @@
 import { h, Component } from 'preact';
-import style from './style.sass';
+import AutoComplete from '../autocomplete';
+import { route } from 'preact-router';
+import nicetime from 'nicetime';
+import formatSize from 'formatsize';
+import Store from 'store';
+import * as CpVault from 'cpvault';
+
+function b64EncodeUnicode(str) {
+	return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
+		return String.fromCharCode('0x' + p1);
+	}));
+}
 
 export default class Home extends Component {
-	render() {
+	constructor() {
+		super();
+
+		this.state = {
+			uploading: false,
+			inputCode: '',
+			inputTitle: '',
+			inputSyntax: null,
+			syntaxes: [],
+			latestPastes: []
+		};
+	}
+
+	componentDidMount() {
+		Store.sideBars.right.title = 'Latest cps';
+		Store.update();
+
+		CpVault.getSyntaxes().then((res) => this.setState({ syntaxes: res.data }));
+		CpVault.getLatestPastes().then(res => {
+			const pastes = res.data;
+			this.setState({ latestPastes: pastes });
+
+			Store.sideBars.right.children = pastes.map(cp => this.makeLatestCpItem(cp));
+			Store.update();
+		});
+	}
+
+	handleCpItemClick(cp) {
+		route(`/${cp.key}`);
+	}
+
+	shortenMoment(str) {
+		return str
+			.replace('seconds', 'sec').replace('second', 'sec')
+			.replace('minutes', 'min').replace('minute', 'min')
+			.replace('hours', 'hr').replace('hour', 'hr');
+	}
+
+	makeLatestCpItem(cp) {
 		return (
-			<div class={style.home}>
-				<h1>Home</h1>
+			<div onClick={() => this.handleCpItemClick(cp)} class="latest-cp">
+				<span>{cp.title}</span>
+				<small>{formatSize(cp.size)}</small>
+				<small>{cp.syntaxName} | {this.shortenMoment(nicetime(cp.createdAt * 1000))}</small>
 			</div>
+		);
+	}
+
+	startUpload = () => {
+		this.setState({ uploading: true });
+
+		const data = {
+			title: this.state.inputTitle,
+			visibility: 0,
+			syntaxId: (this.state.inputSyntax) ? this.state.inputSyntax.id : 1,
+			content: b64EncodeUnicode(this.state.inputCode)
+		};
+
+		CpVault.uploadPaste(data).then(res => {
+			this.setState({
+				uploading: false,
+				inputCode: '',
+				inputTitle: '',
+				inputSyntax: null
+			});
+
+			route('/' + res.data.key);
+		});
+	}
+
+	onInputCodeChange = (e) => {
+		this.setState({ inputCode: e.target.value });
+	}
+
+	onInputTitleChange = (e) => {
+		this.setState({ inputTitle: e.target.value });
+	}
+
+	onInputSyntaxChange = (data) => {
+		console.log(data);
+		this.setState({ inputSyntax: (data) ? data : null });
+	}
+
+	render() {
+		const progressStyle = { visibility: (this.state.uploading) ? "visible" : "hidden" };
+
+		return (
+			<main class="home">
+        <section>
+           <div class="upload-form">
+            <h1>cpVault</h1>
+            <span class="title">Upload a new paste</span>
+            <div class="box">
+              <input type="text" placeholder="Title..." value={this.state.inputTitle} onChange={this.onInputTitleChange} disabled={this.state.uploading} />
+              <AutoComplete placeholder="Syntax highhlighting..." data={this.state.syntaxes} itemNameProp="name" onItemChange={this.onInputSyntaxChange} disabled={this.state.uploading} />
+              <textarea placeholder="Code goes here..." value={this.state.inputCode} onChange={this.onInputCodeChange} disabled={this.state.uploading} />
+              <button onClick={this.startUpload} disabled={this.state.uploading}>Upload</button>
+              <div style={progressStyle} class="progress"><div class="indeterminate" /></div>
+            </div>
+          </div>
+        </section>
+      </main>
 		);
 	}
 }
