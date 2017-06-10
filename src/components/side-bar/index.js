d17fbcd6b00e430399968517a16a76e327edd917
Dávid Szabó <david.szabo97@gmail.com>
2017-06-10 22:33:15 +0200

    Add sidebar component

---
+++ src/components/side-bar/index.js
@@ -0,0 +1,15 @@
+import { h, Component } from 'preact';
+
+export default class SideBar extends Component {
+	render() {
+		let typeClass = (this.props.left) ? ' side-bar-left' : (this.props.right) ? ' side-bar-right' : '';
+
+		return (
+			<aside class={'side-bar' + typeClass}>
+        <span class="title">{this.props.title}</span>
+        <hr />
+				{this.props.children}
+      </aside>
+		);
+	}
+}
