745eb2334fff1e4e78c271bbe7931fc877ff3cc7
Dávid Szabó <david.szabo97@gmail.com>
2017-08-01 12:14:51 +0200

    Add Select component

---
+++ src/components/select/index.js
@@ -0,0 +1,64 @@
+import { h, Component } from 'preact';
+
+export class SelectItem extends Component {
+  onItemClick = () => {
+    this.props.onItemClick(this.props.data);
+  }
+
+  render() {
+    let classes = "select__item";
+    if (this.props.data.active) {
+      classes += " select__item--active";
+    }
+
+    return (<div class={classes} onClick={this.onItemClick}>{this.props.name}</div>);
+  }
+}
+
+export default class Select extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: props.default || this.props.data[0][this.props.itemNameProp]
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.default !== this.props.default) {
+      this.setState({ input: nextProps.default || '' });
+    }
+  }
+
+  onItemClick = data => {
+    this.setState({ input: data[this.props.itemNameProp] });
+
+    if (this.props.onItemClick) {
+      this.props.onItemClick(data);
+    }
+
+    if (this.props.onItemChange) {
+      this.props.onItemChange(data);
+    }
+  }
+
+  createItem(data) {
+    return <SelectItem onItemClick={this.onItemClick} name={data[this.props.itemNameProp]} data={data} />;
+  }
+
+  render() {
+    return (
+      <div class="select">
+        {this.props.data.map(v => {
+          if (this.state.input === v[this.props.itemNameProp]) {
+            v.active = true;
+          } else {
+            v.active = false;
+          }
+
+          return this.createItem(v);
+        })}
+      </div>
+    );
+  }
+}
