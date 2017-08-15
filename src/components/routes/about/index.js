73a7ff3063b15762afa754deb142dd2ff6eb5126
Dávid Szabó <david.szabo97@gmail.com>
2017-08-15 16:12:13 +0200

    Update links

--- src/components/routes/about/index.js
+++ src/components/routes/about/index.js
@@ -7,8 +7,8 @@ export default class About extends Component {
       <main class="default about">
         <section>
           <h1>Open-source project</h1>
-          <p><strong>Github repository API: </strong><a href="https://github.com/david-szabo97/snipphex">https://github.com/david-szabo97/snipphex</a></p>
-          <p><strong>Github repository front-end: </strong><a href="https://github.com/david-szabo97/snipphex-web">https://github.com/david-szabo97/snipphex-web</a></p>
+          <p><strong>Github repository API: </strong><a href="https://github.com/SnippHex/snipphex">https://github.com/SnippHex/snipphex</a></p>
+          <p><strong>Github repository front-end: </strong><a href="https://github.com/SnippHex/snipphex-web">https://github.com/SnippHex/snipphex-web</a></p>
           <p><strong>Version: </strong>{ __VERSION__ }</p>
           <p><strong>Build date: </strong>{ __BUILD_DATE__ }</p>
         </section>
